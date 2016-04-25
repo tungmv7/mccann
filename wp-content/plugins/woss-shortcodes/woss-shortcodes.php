0affb9336e8e01daa1c6075c73ea4141503d6406
Tung Mang Vien <tungmv7@gmail.com>
2016-04-25 21:13:12 +0700

    fix bugs

--- wp-content/plugins/woss-shortcodes/woss-shortcodes.php
+++ wp-content/plugins/woss-shortcodes/woss-shortcodes.php
@@ -1438,6 +1438,12 @@ function th_team_member($atts, $content=false)
                     <!-- Image -->
                     '.$image.'
                 </div>
+                <div class="team-details text-center">
+                    <!-- Info -->
+                    <h3>'.$name.'</h3>
+                    <h5>'.$position.'</h5>
+                    <p>'.$description.'</p>
+                </div>
             </div> <!-- /.row -->';
 
 }
